71deb83fc2b1093b31230072688dd5666725a26a
root <root@itcast.cn>
2019-02-21 19:03:46 +0800

    提交代码01

---
+++ Dd/Dd.java
@@ -0,0 +1,19 @@
+public class Dd{
+	public static void main(String[] args){
+		farmer();
+		seller();
+		cook();
+	}
+	public static void cook(){
+		System.out.println("œ¥≤À");
+		System.out.println("«–≤À");
+	}	
+    public static void seller(){
+		System.out.println("÷÷÷≤");
+		System.out.println("…±≥Ê");
+		System.out.println("–ﬁºÙ");
+	}
+	public static void farmer(){
+		System.out.println("Œ“");
+	}
+}
